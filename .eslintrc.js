db29f152ee007170165a4fa1051a4a1d478f3e37
Nathan Lee <nathanlee3296@hotmail.co.uk>
2024-01-02 14:45:12 +0000

    Update ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: 'airbnb',
+  extends: 'standard',
   overrides: [
     {
       env: {
-        node: true,
+        node: true
       },
       files: [
-        '.eslintrc.{js,cjs}',
+        '.eslintrc.{js,cjs}'
       ],
       parserOptions: {
-        sourceType: 'script',
-      },
-    },
+        sourceType: 'script'
+      }
+    }
   ],
   parserOptions: {
     ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-  },
-};
+  }
+}
